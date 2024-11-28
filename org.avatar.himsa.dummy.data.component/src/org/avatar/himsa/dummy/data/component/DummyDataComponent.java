7cfb0347ae9e56c6c3dbc667508bf241a8fa7c9f
ilenia <ilenia@ilenia-XPS-15-9520>
2024-11-28 08:44:42 +0100

    removed datafaker from workspace and used local jar from gecko.libraries
    Signed-off-by: ilenia <ilenia@ilenia-XPS-15-9520>

--- org.avatar.himsa.dummy.data.component/src/org/avatar/himsa/dummy/data/component/DummyDataComponent.java
+++ org.avatar.himsa.dummy.data.component/src/org/avatar/himsa/dummy/data/component/DummyDataComponent.java
@@ -39,11 +39,11 @@ public class DummyDataComponent {
 	private static final List<String> HEALTH_INSURANCE_COMPANIES_GERMANY = List.of("AOK", "BARMER", "HKK", "hkk health insurance", "KNIGHTSHAFT",
 			"DAK health", "KKH", "Techniker Krankenkasse", "TK", "HEK", "Hanseatic Health Insurance", "BKK", "IKK"); 
 	
-	private Faker faker = new Faker(new Locale("it"));
+	private Faker faker = new Faker(new Locale("de"));
 	private PromiseFactory factory = new PromiseFactory(Executors.newFixedThreadPool(4));
 	
 	@Activate
-	public void activate() {
+	public void activate() {		
 		factory.submit(() -> {
 			doCreateDummyData();
 			return true;
